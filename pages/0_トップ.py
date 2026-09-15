9576b605bac10bb1b98c5cadec05276a2cf2ce76
chemo-pharm-tohoku <mao.maekawa.d3@tohoku.ac.jp>
2026-09-16 07:49:30 +0900

    Update 0_トップ.py

--- pages/0_トップ.py
+++ pages/0_トップ.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="ケモテンプレートシステム",
+    page_title="ケモテンプレート生成システム",
     page_icon="💊",
     layout="centered",
     initial_sidebar_state="expanded",
